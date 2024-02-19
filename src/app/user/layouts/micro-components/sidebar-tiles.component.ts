0cf7a2d8e364e60e10598b3d1bc0feb7c6691670
Rohit1024 <rohitkharche1024@gmail.com>
2024-02-20 03:27:58 +0530

    ""

---
+++ src/app/user/layouts/micro-components/sidebar-tiles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'sidebar-tiles',
+  standalone: true,
+  imports: [RouterModule],
+  template: ``,
+})
+export class SideBarTilesComponent {}
